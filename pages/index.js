b733e9c8e1aa53ed90604504f7260c3613a6e565
JG <diasusb@hotmail.com>
2021-01-26 01:39:41 -0300

    Aula1 - adcionando componentes e estilização da page principal

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,53 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
 
-// function Title(props) {
-//   return (
-//     <h1>Olá, {props.children}</h1>
-//   );
-// }
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <QuizBackground style= {{ backgroundImage: `url(${db.bg})`}} >
+      <title>JavaPorémScript</title>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Olá Jsteiros</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Esse conteudo se dedica ao JS</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Olá visitante</h1>
+            <p>Esse conteudo se dedica ao JS</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/gabrielf7" />
+    </QuizBackground>
+  );
 }
