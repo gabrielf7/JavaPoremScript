f1576478011b1cb777a25de77ad5906634d65106
JG <diasusb@hotmail.com>
2021-01-26 03:12:55 -0300

    teste 5 meta tag

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -2,18 +2,17 @@ export default function Head() {
   return (
     <head>
       <meta charset="UTF-8" />
-      <meta property="url" content="https://javaporemscript.gabrielf7.vercel.app/" />
-      <meta property="url" content="https://javaporemscript.vercel.app/" />
-      <meta property="type" content="Quiz" />
-      <meta property="title" content="JavaPorémScript" />
-      <meta name="description" content="
+      <meta property="og:url" content="https://javaporemscript.vercel.app/" />
+      <meta property="og:type" content="Quiz" />
+      <meta property="og:title" content="JavaPorémScript" />
+      <meta name="og:description" content="
         JavaPorémScript 
         - 
         Site destinado ao aprendizado da linguaguem de programação Javascript, com inuito de apresentar um Quiz sobre Javascript.
       " />
       <meta name="keywords" content="JavaScript and Next.js" />
       <meta name="author" content="João Gabriel" />
-      <meta property="image" content="https://raw.githubusercontent.com/gabrielf7/javaporemscript/main/src/assets/EzgdmaCQuT84bgDL4fhXZS.jpg" />
+      <meta property="og:image" content="https://raw.githubusercontent.com/gabrielf7/javaporemscript/main/src/assets/EzgdmaCQuT84bgDL4fhXZS.jpg" />
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="800" />
       <meta property="og:image:height" content="600" />
