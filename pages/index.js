994cf79918cd50bcdf13721dd0927a46931ed00c
JG <diasusb@hotmail.com>
2021-01-28 19:46:32 -0300

    Finalizando segunda etapa de entendimento.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import MyHead from '../src/components/MyHead';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
@@ -21,6 +23,24 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+  // console.log('Retorno do UseState: ', name, setName);
+
+  function submeter(infoForm) {
+    infoForm.preventDefault();
+    router.push(`/quiz?name=${name}`);
+
+    // console.log('Dados pegados do input');
+    // router manda para a próxima página
+  }
+
+  function mudar(mudarInfo) {
+    // console.log(mudarInfo.target.value);
+    // State
+    setName(mudarInfo.target.value);
+  }
+
   return (
     <>
       <Head>
@@ -35,7 +55,17 @@ export default function Home() {
               <h1>Olá Jsteiros</h1>
             </Widget.Header>
             <Widget.Content>
-              <p>Esse conteudo se dedica ao JS</p>
+              <form onSubmit={submeter(this)}>
+                <input
+                  onChange={mudar(this)}
+                  type="text"
+                  placeholder="Digite seu nome"
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Jogar - 
+                  {name}
+                </button>
+              </form>
             </Widget.Content>
           </Widget>
           <Widget>
