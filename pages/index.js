933a0e69b16480faf17fd1a558696448112df225
JG <diasusb@hotmail.com>
2021-01-28 21:51:19 -0300

    erro no vercel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 import MyHead from '../src/components/MyHead';
 import db from '../db.json';
@@ -23,23 +23,23 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
   const [name, setName] = useState('');
   console.log('Retorno do UseState: ', name, setName);
 
-  function submeter(infoForm) {
-    infoForm.preventDefault();
-    router.push(`/quiz?name=${name}`);
+  // function submeter(infoForm) {
+  //   infoForm.preventDefault();
+  //   router.push(`/quiz?name=${name}`);
 
-    // console.log('Dados pegados do input');
-    // router manda para a próxima página
-  }
+  //   console.log('Dados pegados do input');
+  //   // router manda para a próxima página
+  // }
 
-  function mudar(mudarInfo) {
-    // console.log(mudarInfo.target.value);
-    // State
-    setName(mudarInfo.target.value);
-  }
+  // function mudar(mudarInfo) {
+  //   // console.log(mudarInfo.target.value);
+  //   // State
+  //   // setName(mudarInfo.target.value);
+  // }
 
   return (
     <>
@@ -55,9 +55,9 @@ export default function Home() {
               <h1>Olá Jsteiros</h1>
             </Widget.Header>
             <Widget.Content>
-              <form onSubmit={submeter(this)}>
+              <form onSubmit="submeter(this)">
                 <input
-                  onChange={mudar(this)}
+                  onChange="mudar(this)"
                   type="text"
                   placeholder="Digite seu nome"
                 />
